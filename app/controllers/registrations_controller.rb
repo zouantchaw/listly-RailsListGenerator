441eecb9b4380d13fee323b53218a133dcaee4d1
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-06-16 14:15:59 -0400

    First Commit

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationsController < Devise::RegistrationsController 
+    private 
+
+    def signup_params 
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end 
+
+    def account_update_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end 
+end
