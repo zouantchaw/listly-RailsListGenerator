2313a5041be2bda238406252f36dab03d2e3e8a7
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-06-16 20:56:55 -0400

    Devise auth complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :users, :controllers => {:registrations => 'registrations'}
+  devise_for :users, :controllers => {:registrations => 'registrations', :omniauth_callbacks => 'callbacks'}
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new', as: 'login'
+    get 'signup', to: 'devise/registrations#new', as: 'signup'
+  end
+
   root to: 'application#welcome'
 end
