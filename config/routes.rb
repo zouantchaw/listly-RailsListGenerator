535279ebf1afa4c22305c1b323d3bbb4f6aa6e47
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-06-21 16:42:24 -0400

    new path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :cards
   resources :lists
+
+  resources :lists, only: [:show, :index] do
+    resources :cards, only: [:new]
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, :controllers => {:registrations => 'registrations', :omniauth_callbacks => 'callbacks'}
 
