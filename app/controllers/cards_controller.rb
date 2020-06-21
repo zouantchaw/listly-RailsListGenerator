d4016462451ec8efe583cd346b49e6faa42067f3
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-06-21 18:45:14 -0400

    cards edit

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -18,6 +18,12 @@ class CardsController < ApplicationController
         end
     end 
 
+    def edit 
+        binding.pry
+        list = current_user.lists.find_by(id: params[:list_id])
+        @card = list.cards.find_by(id: params[:id])
+    end 
+
     private 
 
     def card_params 
