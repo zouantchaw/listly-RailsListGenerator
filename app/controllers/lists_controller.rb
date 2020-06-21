692bf7838fb624c7d702734bf4766fec2770af62
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-06-21 13:35:34 -0400

    List controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,47 @@ class ListsController < ApplicationController
     end 
 
     def show 
-        
+        @list = current_user.lists.find(params[:id])
+    end 
+
+    def new 
+        @list = current_user.lists.build
+    end 
+
+    def create 
+        @list = current_user.lists.create(list_params)
+
+        if @list.save
+            redirect_to @list
+        else 
+            render :new
+        end 
+    end 
+
+    def edit 
+        @list = current_user.lists.find(params[:id])
+    end 
+
+    def update 
+        @list = current_user.lists.find(params[:id])
+        @list.update(list_params)
+        if @list.save 
+            redirect_to @list 
+        else 
+            render :edit 
+        end 
+    end 
+
+    def destroy 
+        @list = current_user.lists.find(params[:id])
+        @list.destroy 
+        flash[:notice] = "List deleted."
+        redirect_to lists_path
+    end 
+
+    private 
+
+    def list_params
+        params.require(:list).permit(:title)
     end 
 end
