c6dca3c44663fa6a143979b73410fd9a0b20cf7d
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-06-21 20:08:35 -0400

    edit security

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -19,7 +19,18 @@ class CardsController < ApplicationController
     end 
 
     def edit 
-        list = current_user.lists.find_by(id: params[:list_id])
+        binding.pry
+        if params[:list_id]
+            list = current_user.lists.find_by(id: params[:list_id])
+            if list.nil?
+                redirect_to lists_path, alert: "List not Found."
+            else
+                @card = list.cards.find_by(id: params[:id])
+                redirect_to list_cards_path(list), alert: "Post not found." if @card.nil?
+            end
+        else 
+            @card = Card.find(params[:id]) #I dont need this. I should def take it out.
+        end 
         @card = list.cards.find_by(id: params[:id])
     end 
 
