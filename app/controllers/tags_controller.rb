13dff36d2fd9c342f4c57222506bf51257c452a7
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-07-14 10:44:55 -0400

    Tag Update

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,13 +1,45 @@
 class TagsController < ApplicationController
-    def show 
-
-    end 
 
     def new 
-        binding.pry
-    end 
+        if params[:list_id] && !List.exists?(params[:list_id])
+            redirect_to lists_path, alert: "List not found."
+        else 
+            @list = current_user.lists.find_by(id: params[:list_id])
+            @tag = @list.tags.build
+        end    
+    end  
 
     def create
+        @list = current_user.lists.find_by(id: params["list_id"].to_i)
+        @tag = @list.tags.create(tag_params)
+        if @tag.save
+            redirect_to @list
+        else 
+            render :new 
+        end
+    end 
+
+    def edit 
+        @tag = Tag.find(params[:id])
+    end 
+
+    def update 
+        @tag = Tag.find(params[:id])
+
+        @tag.update(tag_params)
+
+        if @tag.save
+            redirect_to list_path
+        else 
+            render :edit
+        end 
+    end 
+
+    def destroy 
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+        flash[:notice] = "Tag Deleted"
+        redirect_to list_path
 
     end 
 
